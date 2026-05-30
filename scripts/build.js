3978c3195747ec31e82f346e0bf20730874fe2af
Isaiah McCray <isaiahmccray8@gmail.com>
2026-05-30 13:29:17 -0700

    use node script to copy frontend to public

---
+++ scripts/build.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const src = path.join(__dirname, '..', 'frontend');
+const dest = path.join(__dirname, '..', 'public');
+
+function copyDir(from, to) {
+  fs.mkdirSync(to, { recursive: true });
+  for (const entry of fs.readdirSync(from, { withFileTypes: true })) {
+    const srcPath = path.join(from, entry.name);
+    const destPath = path.join(to, entry.name);
+    if (entry.isDirectory()) {
+      copyDir(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
+
+copyDir(src, dest);
+console.log('Frontend copied to public/');
